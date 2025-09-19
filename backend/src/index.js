8457ffe8f9cc99993449f817d8fda898352299dd
Lakshya080805 <lakshyasinghal08@gmail.com>
2025-09-19 23:42:25 +0530

    c

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,7 +12,7 @@ import messageRoutes from "./routes/message.route.js"
 import { app,server } from "./lib/socket.js";
 
 dotenv.config();
-const PORT=process.env.PORT;
+const PORT=process.env.PORT || 5000;
 const __dirname=path.resolve();
 
 
@@ -30,7 +30,7 @@ app.use("/api/messages",messageRoutes)
 if(process.env.NODE_ENV==="production"){
     app.use(express.static(path.join(__dirname,"../frontend/dist")));
 
-    app.get("/:path(*)",(req,res)=>{
+    app.get("*",(req,res)=>{
         res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
 
     })
