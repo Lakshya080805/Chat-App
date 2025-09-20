d0d363fec94c4538fe02f94dc4b178750331ca57
Lakshya080805 <lakshyasinghal08@gmail.com>
2025-09-20 22:40:11 +0530

    .

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,21 @@
-import axios from "axios";
+// import axios from "axios";
 
-export const axiosInstance=axios.create({
-    baseURL : import.meta.env.MODE==="development"?"https://chat-app-2-2qpk.onrender.com":"/api",
-    withCredentials: true,
+// export const axiosInstance=axios.create({
+//     baseURL : import.meta.env.MODE==="development"?"https://chat-app-2-2qpk.onrender.com":"/api",
+//     withCredentials: true,
 
-})
+// })
 
 //http://localhost:5000/api
+
+import axios from "axios";
+
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5000/api" // local backend
+    : "https://chat-app-2-2qpk.onrender.com/api"; // deployed backend
+
+export const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  withCredentials: true,
+});
