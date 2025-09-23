fb713f1861ad78218045f1345b7d1268c423bbba
Lakshya080805 <143376209+Lakshya080805@users.noreply.github.com>
2025-09-23 09:10:09 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -19,3 +19,15 @@ export const axiosInstance = axios.create({
   baseURL: BASE_URL,
   withCredentials: true,
 });
+
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token'); // Get token from localStorage
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`; // Attach token
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
