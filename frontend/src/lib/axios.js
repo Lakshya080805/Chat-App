3930d3e836799265c83ac2797f5509c1eb29c4cc
Lakshya080805 <lakshyasinghal08@gmail.com>
2025-09-23 16:38:32 +0530

    issue

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -10,10 +10,12 @@
 
 import axios from "axios";
 
-const BASE_URL =
-  import.meta.env.MODE === "development"
-    ? "http://localhost:5000/api" // local backend
-    : "https://chat-app-2-2qpk.onrender.com/api"; // deployed backend
+// const BASE_URL =
+//   import.meta.env.MODE === "development"
+//     ? "http://localhost:5000/api" // local backend
+//     : "https://chat-app-2-2qpk.onrender.com/api"; // deployed backend
+
+const BASE_URL = "";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
