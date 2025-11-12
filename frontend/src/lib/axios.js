87e9a97087173a774aa8b0812f48d8c3e5216c73
Lakshya080805 <lakshyasinghal08@gmail.com>
2025-11-12 19:54:27 +0530

    env files

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,19 @@
+// import axios from "axios";
+
+// export const axiosInstance=axios.create({
+//     baseURL : import.meta.env.MODE==="development"?"http://localhost:5000/api":"/api",
+//     withCredentials: true,
+
+// })
+
 import axios from "axios";
 
-export const axiosInstance=axios.create({
-    baseURL : import.meta.env.MODE==="development"?"http://localhost:5000/api":"/api",
-    withCredentials: true,
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5000"
+    : import.meta.env.VITE_BACKEND_URL; // ✅ comes from Vercel env var
 
-})
+export const axiosInstance = axios.create({
+  baseURL: `${BASE_URL}/api`,
+  withCredentials: true,
+});
